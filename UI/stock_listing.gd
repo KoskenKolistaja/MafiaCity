86d4f300f1b8a15913ad73a5bbe0296dd91eadf4
KoskenKolistaja <touko2000@gmail.com>
2025-08-09 22:06:07 +0300

    Better stock window

--- UI/stock_listing.gd
+++ UI/stock_listing.gd
@@ -16,8 +16,8 @@ func update_data():
 	$Panel/CompanyName.text = company.name
 	$Panel2/StockValue.text = str(CompanyManager.get_share_value(company_id))+"🪙"
 	var share_numbers = str(CompanyManager.get_owned_shares(multiplayer.get_unique_id(),company_id)) + "/" + str(CompanyManager.get_total_shares(company_id))
-	$Panel3/OwnedAmount.text = "Owned: " + share_numbers
-
+	$Panel3/OwnedAmount.text = share_numbers
+	$Panel4/AvailableAmount.text = str( int(CompanyManager.companies[company_id].shareholders[0]))
 
 func _on_buy_button_pressed():
 	buy()
